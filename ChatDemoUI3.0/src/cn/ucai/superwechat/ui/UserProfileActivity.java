242e5fe695d0eb42d8f9ece45b23f56d6409e115
cgdfdy <877491315@qq.com>
2016-11-05 14:27:28 +0800

    更新用户头像 修改布局bug

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/UserProfileActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/UserProfileActivity.java
@@ -24,13 +24,19 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.hyphenate.EMValueCallBack;
 import com.hyphenate.easeui.domain.EaseUser;
+import com.hyphenate.easeui.utils.EaseImageUtils;
 import com.hyphenate.easeui.utils.EaseUserUtils;
 
+import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.ucai.superwechat.I;
 import cn.ucai.superwechat.R;
 import cn.ucai.superwechat.SuperWeChatHelper;
 import cn.ucai.superwechat.bean.Result;
@@ -270,7 +276,7 @@ public class UserProfileActivity extends BaseActivity implements OnClickListener
     }
 
     private void uploadUserAvatar(final byte[] data) {
-        dialog = ProgressDialog.show(this, getString(R.string.dl_update_photo), getString(R.string.dl_waiting));
+        
         new Thread(new Runnable() {
 
             @Override
@@ -304,10 +310,10 @@ public class UserProfileActivity extends BaseActivity implements OnClickListener
         return baos.toByteArray();
     }
 
-    @OnClick({R.id.btn_back, R.id.layout_avatar, R.id.layout_profile_nick, R.id.layout_weixinhao})
+    @OnClick({R.id.ivBack,R.id.layout_avatar, R.id.layout_profile_nick, R.id.layout_weixinhao})
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.btn_back:
+            case R.id.ivBack:
                 MFGT.finish(this);
                 break;
             case R.id.layout_avatar:
@@ -339,5 +345,24 @@ public class UserProfileActivity extends BaseActivity implements OnClickListener
                 CommonUtils.showMsgShortToast(R.string.User_name_cannot_be_modify);
                 break;
         }
+    }
+    public File saveBitmapFile(Intent picdata){
+        Bundle extras = picdata.getExtras();
+        if (extras != null){
+            Bitmap bitmap = extras.getParcelable("data");
+            String imagePath = EaseImageUtils.getImagePath(user.getMUserName()+ I.AVATAR_SUFFIX_JPG);
+            File file = new File(imagePath);
+            L.e("file path="+file.getAbsolutePath());
+            try {
+                BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
+                bitmap.compress(Bitmap.CompressFormat.PNG,100,bos);
+                bos.flush();
+                bos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return file;
+        }
+        return null;
     }
 }
